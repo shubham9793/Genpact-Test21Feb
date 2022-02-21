ff992f06f66a3aa013e67de9795ad7e8c63ab5f9
shubham sahoo <56964646+shubham9793@users.noreply.github.com>
2022-02-21 11:25:41 +0530

    Update main2.java

--- main2.java
+++ main2.java
@@ -1,45 +1,39 @@
-
-package com.org.gen.TestAssignment21Feb;
-
 import java.util.Scanner;
+class UserMainCode{
+    public static int validatePassword(String a){
+        int d = 0;
+        if (a.length() >= 8) {
+            if (a.contains("#") || a.contains("@") || a.contains("_")) {
+                char c = a.charAt(0);
+                //System.out.println(c);
+                if (Character.isAlphabetic(c)) {
+                    char dd = a.charAt(a.length() - 1);
+                    if ((Character.isAlphabetic(dd)) || (Character.isDigit(dd))){
+                        if (a.matches(".[0-9]{1,}.") || a.matches(".[a-zA-Z]{1,}."))
+                            return 1;
+                        else
+                            return -1;
+                    }
+                    else
+                        return -1;
+                }
+                else
+                    return -1;
+            } else
+                return -1;
 
-public class main2 {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter Pass Type ");
-    String a = sc.next();
-    int d = 0;
-    if (a.length() >= 8) {
-      if (a.contains("#") || a.contains("@") || a.contains("_")) {
-        char c = a.charAt(0);
-     
-        if (Character.isAlphabetic(c)) {
-          char dd = a.charAt(a.length() - 1);
-          
-          if ((Character.isAlphabetic(dd)) || (Character.isDigit(dd)))
-
-          {
-            if (a.matches(".*[0-9]{1,}.*")
-                || a.matches(".*[a-zA-Z]{1,}.*")) {
-
-              System.out.println("Valid");
-
-            } else{
-              System.out.println("Not Valid");
-            }
-          } else{
-            System.out.println("Not Valid");
-          }
-        }
-         else{
-            System.out.println("Not Valid");
-          }
-      } else{
-        System.out.println("Not Valid");
-      }
-
-    } else{
-      System.out.println("Not Valid");
+        } else
+            return -1;
     }
-  }
+}
+public class main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.pritln("Enter Pass type");
+        String str = sc.next();
+        if(UserMainCode.validatePassword(str) == 1)
+            System.out.println("Valid");
+        else
+            System.out.println("Not valid");
+    }
 }
